f3ed34fe717fda748eca7632f7bcef966cf67ca1
Karan Bhogal <kbhog001@ucr.edu>
2022-12-06 13:04:41 -0800

    Working Forward Selection

--- Project2.py
+++ Project2.py
@@ -1,23 +1,23 @@
 import numpy as np
 import random as rd
-#kannon = np.array([2,4,6])
-#print(kannon)
-#Calculates the distance between 2 rows
+import copy as dp
 
-
-def calculate_euclideanDistance(row1, row2, current_set):
+def calculate_euclideanDistance(row1, row2, current_set:list ):
     distance = .0
-    for num in len(row1):
-        if num not in current_set:
-            row1[num] = 0
-            row2[num] = 0
-    for i in range(1, len(row1)):
-        distance = distance + pow(row1[i] - row2[i],2)
+    #print("Feature List: ", current_set)
+    #print("Row 1: ", row1)
+    #print("Row 2: ", row2)
+    for i in range(1,len(row1)+1):
+        #print ("I value: ", i)
+        if i in current_set:
+            #print("Row 1 i: " + str(i) + "Row 1 at i = " + str(row1[i-1]))
+            #print("Row 2 i: " + str(i) + "Row 2 at i = " + str(row2[i-1]))
+            distance = distance + pow(row1[i-1] - row2[i-1],2)
     distance = np.sqrt(distance)
     return distance
 
 #Make an list of the data and find closest neighbors
-def find_Neighbors(data, current_set, feature_to_add):
+def find_Neighbors(data, current_set: list, feature_to_add):
     distanceList = []
     neighborList = []
     numCount = 0
@@ -46,26 +46,42 @@ def find_Neighbors(data, current_set, feature_to_add):
 def calculateProbability(data, currentSet):
     totalNum = data.size/data[1].size
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
+def leave_one_out_cross_validation(data, current_set: list, feature_to_add):
     number_correctly_classified = 0
+    print("Current Set looking like: ", current_set)
+    feature_set = []
+    for i in range(len(current_set)):
+        feature_set.append(current_set[i])
+    feature_set.append(feature_to_add)
+    print("Feature Set to add looking like: ", feature_set)
     for i in range(len(data)):
         object_to_classify = data[i][1:]
         label_object_to_classify = data[i][0]
-        #print("Row: ", object_to_classify)
-        #print("Objects to classify", label_object_to_classify)
         nearest_neighbor_distance = 999
         nearest_neighbor_location = 999
-        print("Looping over i, at the " + str(i) + " location")
-        print("The "+ str(i) + "th object is in class " + str(label_object_to_classify))
+        #print("Looping over i, at the " + str(i) + " location")
+        #print("The "+ str(i) + "th object is in class " + str(label_object_to_classify))
         for j in range(len(data)):
             if j != i:
-                dist = calculate_euclideanDistance(object_to_classify, data[j][1:], current_set)
-                
-    return rd.random()%100
+                dist = calculate_euclideanDistance(object_to_classify, data[j][1:], feature_set)
+                #dist = np.sqrt(sum((object_to_classify - data[j][1:])**2))
+                if dist < nearest_neighbor_distance:
+                    nearest_neighbor_distance = dist
+                    nearest_neighbor_location = j
+                    nearest_neighbor_label = data[j][0]
+        #print("Object " + str(i) + " is class " + str(label_object_to_classify))
+        #print("Its nearest neighbor is " + str(nearest_neighbor_location) + " which is in class " + str(nearest_neighbor_label))
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified = number_correctly_classified + 1
+    accuracy = number_correctly_classified/len(data)
+    print("Accuracy: ", accuracy)
+    return accuracy #rd.random()%100
 
 
 def feature_search_demo(data):
     current_set = []
+    final_set = []
+    final_accuracy = 0
 
     for i in range (1,len(data[0])):
         print("On the " + str(i) + "th level of the search tree")
@@ -73,51 +89,31 @@ def feature_search_demo(data):
         best_accuracy_so_far = 0
 
         for j in range (1,len(data[0])):
+            print("J value: ", j)
             if j not in current_set:
                 print("--Considering adding the " + str(j) + " feature")
                 accuracy = leave_one_out_cross_validation(data, current_set, j)
-        
-            if accuracy > best_accuracy_so_far:
-                best_accuracy_so_far = accuracy
-                feature_to_add_on_this_level = j
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add_on_this_level = j
 
         print("On level " + str(i) + ", I added feature " + str(feature_to_add_on_this_level) + " to current set\n")
         current_set.append(feature_to_add_on_this_level)
+        if best_accuracy_so_far >= final_accuracy:
+            final_accuracy = best_accuracy_so_far
+            final_set.append(feature_to_add_on_this_level)
+    
+    print("Best set: ", final_set)
+    print("Final Accuracy: ", final_accuracy)
+
+
 
 
 print("Testing Output")
-data = np.loadtxt('CS170_Small_Data__48.txt')
+data = np.loadtxt('CS170_Small_Data__6.txt')
 #print(data[])
 current_set = []
 numOfFeatures = data[1].size - 1
 
-dataset = [[0,4,8],
-	[0,10,5],
-    [0, 5, 9],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
-
-dataset1 = [[2,4,8],
-	[2,10,5],
-    [1, 5, 9],
-    [2, 3, 7],
-    [1, 142, 8]]
-
-leave_one_out_cross_validation(data, 0, 0)
-
-"""
-row0 = dataset1[0]
-#for row in dataset:
-	#distance = calculate_euclideanDistance(row0, row)
-	#print(distance)
-print(numOfFeatures)
-find_Neighbors(dataset1, row0, 3)
-#print(data.size/data[0].size)
 feature_search_demo(data)
-"""
+
